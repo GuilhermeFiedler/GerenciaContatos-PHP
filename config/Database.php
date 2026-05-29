522acaac023a82cff7ec09d21e2ed1120f88c482
GuilhermeFiedler <lhermetfied@gmail.com>
2026-05-29 14:49:37 -0300

    WIP: escopo da database.

---
+++ config/Database.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class Database
+{
+    private static ?PDO $instance = null;
+
+    public static function getConnection(): PDO
+    {
+        if (self::$instance == null) {
+            self::$instance = new PDO(dsn: "pgsql:host=" . getenv('DB_HOST') . ";port=" . getenv('DB_PORT') . ";dbname=" . getenv('DB_NAME'),
+                username: $_ENV('DB_USER'),
+                password: $_ENV('DB_PASS'),
+                options: [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO:: ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_STRINGIFY_FETCHES => false,
+                    PDO::ATTR_EMULATE_PREPARES => false
+                ],
+            );
+        }
+        return self::$instance;
+    }
+}
