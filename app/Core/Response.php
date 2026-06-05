1e03b1608fae3609243b2cb2cd22191d0d5ec4f0
GuilhermeFiedler <lhermetfied@gmail.com>
2026-06-05 13:49:32 -0300

    feat: Response de sucesso e erro.

--- app/Core/Response.php
+++ app/Core/Response.php
@@ -1,10 +1,28 @@
 <?php
 
 class Response {
-    public static function json($data, $status = 200){
+    public static function success(
+        mixed $data,
+        int $status = 200
+    ): never {
+
+    http_response_code($status);
+
+    echo json_encode($status);
+
+    echo json_encode([
+        'success' => true,
+        'data' => $data
+    ]);
+
+    exit;
+
+    }
+
+    public static function error(string $message, int $status = 400): never {
         http_response_code($status);
-        header('Content-Type: application/json');
-        echo json_encode($data);
-        exit;
+
+        echo json_encode(['success' => false, 'message' => $message]);
+    exit;
     }
 }
