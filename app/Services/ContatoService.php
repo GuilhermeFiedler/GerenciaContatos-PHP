32a70792de7a27b5dc2dda3cd5742cad8cb6558d
GuilhermeFiedler <lhermetfied@gmail.com>
2026-06-05 15:22:15 -0300

    feat: adiciona camada de serviço de validator e contato.

---
+++ app/Services/ContatoService.php
@@ -0,0 +1,70 @@
+<?php
+
+class ContatoService {
+    public function __construct(
+        private readonly ContatoRepository $repository
+    ) {}
+
+    public function criar(array $dados): array
+    {
+        $nome = trim($dados['nome'] ?? '');
+        $email = trim($dados['email'] ?? '');
+        $telefone = preg_replace('/\D/','',$dados['telefone'] ?? '');
+
+        if ($nome === ''){
+            throw new InvalidArgumentException('Nome é obrigatório');
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            Throw new InvalidArgumentException('E-mail inválido');
+        }
+
+        if (!preg_match('/^\d{10,11}', $telefone)) {
+            throw new InvalidArgumentException('Telefone inválido');
+        }
+
+        $id = $this->repository->criarContato(
+            $nome,
+            $email,
+            $telefone
+        );
+
+        return $this->repository->buscarPorId($id);
+        }
+
+    public function atualizar(int $id, array $dados): bool {
+        $nome = trim($dados['nome'] ?? '');
+        $email = trim($dados['email'] ?? '');
+        $telefone = preg_replace('/\D/','', $dados['telefone'] ?? '');
+
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            throw new InvalidArgumentException('E-mail inválido');
+        }
+
+        if (!preg_match('/^\d{10,11}$/', $telefone)) {
+            throw new InvalidArgumentException('Telefone inválido');
+        }
+
+        return $this->repository->atualizar(
+            $id,
+            $nome,
+            $email,
+            $telefone
+        );
+    }
+
+    public function excluir(int $id): bool {
+        return $this->repository->excluir($id);
+    }
+
+    public function buscar(int $id): ?array {
+        return $this->repository->buscarPorId($id);
+    }
+
+    public function listar(int $page, int $limit): array {
+        return $this->repository->listar(
+            $page,
+            $limit
+        );
+    }
+}
---
+++ app/Services/ValidatorService.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+class ValidatorService
+{
+    public static function email(string $email): bool
+{
+    return filter_var(
+        $email,
+        FILTER_VALIDATE_EMAIL
+    ) !== false;
+}
+    public static function telefone(string $telefone): bool {
+
+    $telefone = preg_replace(
+        '/\D/',
+        '',
+        $telefone
+    );
+
+    return preg_match(
+        '/^\d{10,11}$/',
+        $telefone
+    );
+}
+}
